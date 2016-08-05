bc977a3b627e82844549cb63d27254fb936d234e
lance-parish <lance.parish@gmail.com>
2016-08-04 23:15:31 -0400

    adding controller

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 var app = angular.module('app', []);
+app.controller("MadLibsController", MadLibsController);
 
+function MadLibsController($scope) {
+  $scope.male_name = '{{male name}}';
+  $scope.job_title = '{{job_title}}';
+  $scope.female_name = '{{female_name}}';
+  $scope.tedious_task = '{{tedious_task}}';
+  $scope.useless_skill = '{{ useless_skill }}';
+  $scope.alias_name = '{{robot_name}';
+  $scope.times_faster = '{{times_faster}';
+}
+MadLibsController.$inject = ['$scope'];
+//clear input on focus
 var input = document.querySelectorAll("input");
-for(var i=0; i< input.length;i++)
-{
+for (var i = 0; i < input.length; i++) {
   console.log(input[i]);
   var ele = input[i];
-
   ele.onfocus = delegate(ele, window.clearInput);
 }
- 
- function delegate(obj, handler) {
+
+function delegate(obj, handler) {
   return function () {
     handler.call(obj);
   }
@@ -17,5 +27,5 @@ for(var i=0; i< input.length;i++)
 
 function clearInput() {
   //console.log('zzzzzzzz' + this.value);
-  this.value='';
+  this.value = '';
 }
